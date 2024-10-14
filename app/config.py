892826bcab48e144b32d566721af84a644e3e846
islombeknv <islomjon2702@gmail.com>
2024-10-14 11:13:33 +0500

    update minor things

--- app/config.py
+++ app/config.py
@@ -3,7 +3,7 @@ from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     database_hostname: str = 'localhost'
-    database_port: int = 5432
+    database_port: str = '5432'
     database_password: str = 'postgres'
     database_name: str = 'fastapi'
     database_username: str = 'postgres'
@@ -11,5 +11,7 @@ class Settings(BaseSettings):
     algorithm: str = 'HS256'
     access_token_expire_minuts: int = 30
 
+    class Config:
+        env_file = ".env"
 
 settings = Settings()

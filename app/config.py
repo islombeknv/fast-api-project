44ae9fb5317612f21e678ba548d06d36fe243958
islombeknv <islomjon2702@gmail.com>
2024-10-14 11:07:20 +0500

    update minor things

--- app/config.py
+++ app/config.py
@@ -3,7 +3,7 @@ from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     database_hostname: str = 'localhost'
-    database_port: int = 5432
+    database_port: str = '5432'
     database_password: str = 'postgres'
     database_name: str = 'fastapi'
     database_username: str = 'postgres'
